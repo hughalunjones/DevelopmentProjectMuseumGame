3eff59eb9ceee7e5db1151ad46e807ad0ea11b6d
Hugh Alun-Jones <hughalunjones12@gmail.com>
2021-03-25 18:41:34 +0000

    Digsite is now more dynamic and ready to be filled with new artefacts and background

--- Assets/Scripts/Monobehaviours/GridManager.cs
+++ Assets/Scripts/Monobehaviours/GridManager.cs
@@ -8,9 +8,14 @@ public class GridManager : MonoBehaviour
     [SerializeField]private int cols = 5;
     [SerializeField]private int layers = 3;
     [SerializeField]int tileSize = 1;
+    public GameObject[] artefactArray;
     public GameObject artefactPrefab;
+    public Camera cam;
+    int randomBg;
 
     void Start(){
+        randomBg = Random.Range(1, 3);
+        GenerateBackground();
         GenerateGrid();
         PlaceArtefact();
     }
@@ -20,12 +25,27 @@ public class GridManager : MonoBehaviour
      *    - Different tiles.
      *
      */
+    void GenerateBackground() {
+        GameObject referenceBgDesert = (GameObject)Instantiate(Resources.Load("DesertDigSite"));
+        GameObject referenceBgPermafrost = (GameObject)Instantiate(Resources.Load("PermafrostDigSite"));
+        if (randomBg == 1 || randomBg == 3) {
+            GameObject background = (GameObject)Instantiate(referenceBgDesert, transform);
+            background.transform.position = new Vector3(0, 3.4f, 0);
+        }
+        else if(randomBg == 2) {
+            GameObject background = (GameObject)Instantiate(referenceBgPermafrost, transform);
+            background.transform.position = new Vector3(0, 3.4f, 0);
+        }
+        Destroy(referenceBgDesert);
+        Destroy(referenceBgPermafrost);
+    }
     void GenerateGrid() {
         // Loop for the number of layers in the digsite
         for(int layer = 0; layer < layers; layer++) {
             // Load the reference tiles for the dig spots
             GameObject referenceTileOne = (GameObject)Instantiate(Resources.Load("DirtTileOne"));
             GameObject referenceTileTwo = (GameObject)Instantiate(Resources.Load("DirtTileTwo"));
+            GameObject referenceTileThree = (GameObject)Instantiate(Resources.Load("DirtTileThree"));
             GameObject referenceTileRock = (GameObject)Instantiate(Resources.Load("RockTileOne"));
             // Loop through the number of columns and rows.
             for (int row = 0; row < rows; row++) {
@@ -34,7 +54,7 @@ public class GridManager : MonoBehaviour
                     int randomRock = Random.Range(1, 8);
                     // Bottom layer is 'undiggable'
                     if (layer == (layers - 1)) {
-                        GameObject tile = (GameObject)Instantiate(referenceTileOne, transform);
+                        GameObject tile = (GameObject)Instantiate(referenceTileThree, transform);
                         tile.GetComponent<DigSpotManager>().isDiggable = false;
                         float posX = this.transform.position.x + (col * tileSize);
                         float posY = this.transform.position.y + (row * -tileSize);
@@ -66,13 +86,24 @@ public class GridManager : MonoBehaviour
             }
             Destroy(referenceTileOne);
             Destroy(referenceTileTwo);
+            Destroy(referenceTileThree);
             Destroy(referenceTileRock);
         }
     }
     // Place the artefact within the confines of the grid.
-      void PlaceArtefact() {
+    void PlaceArtefact() {
+        if (randomBg == 1) {
+            artefactPrefab = artefactArray[1];
+        }
+        else if (randomBg == 2) {
+            artefactPrefab = artefactArray[0];
+        }
+        else if (randomBg == 3) {
+            artefactPrefab = artefactArray[1];
+        }
         Vector3 artefactPos = new Vector3(Random.Range((float)(this.transform.position.x), (float)(this.transform.position.x + 4f)), Random.Range((float)(this.transform.position.y), (float)(this.transform.position.y - 4f)), 1.8f);
-        GameObject artefact = (GameObject)Instantiate(artefactPrefab, transform);
+        GameObject artefact = (GameObject)Instantiate(artefactPrefab, transform.position, Quaternion.Euler(0,0,Random.Range(0,360)));
         artefact.transform.position = artefactPos;
-    }
+        artefact.transform.SetParent(transform);
+    }   
 }
